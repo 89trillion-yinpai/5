e54592b0a3a382512cff2406d971fe2bdd1e08d0
尹派 <yinpai@89trillion.com>
2021-11-08 12:34:42 +0800

    feat：第五题代码规范

--- Assets/Function1/03.Scripts/Controller/JiaFenController.cs
+++ Assets/Function1/03.Scripts/Controller/JiaFenController.cs
@@ -242,7 +242,7 @@ namespace Assets.Function1._03.Scripts.Controller
             }
         }
 
-        public void Award()
+        private void Award()
         {
             if (I == 4200)
             {
