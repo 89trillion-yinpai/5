e1f72de938b37accc6f309f7bcf9d1ad6c54fe04
尹派 <yinpai@89trillion.com>
2021-11-08 14:54:07 +0800

    feat：删除无用代码

--- Assets/Function1/03.Scripts/Model/Read.cs
+++ Assets/Function1/03.Scripts/Model/Read.cs
@@ -7,58 +7,54 @@ namespace Assets.Function1._03.Scripts.Model
     public class Read : MonoBehaviour
     {
         //声明8个奖励按钮
-        [SerializeField] protected Button Receive;
-        [SerializeField] protected Button Receive1;
-        [SerializeField] protected Button Receive2;
-        [SerializeField] protected Button Receive3;
-        [SerializeField] protected Button Receive4;
-        [SerializeField] protected Button Receive5;
-        [SerializeField] protected Button Receive6;
-        [SerializeField] protected Button Receive7;
+        protected Button Receive;
+        protected Button Receive1;
+        protected Button Receive2;
+        protected Button Receive3;
+        protected Button Receive4;
+        protected Button Receive5;
+        protected Button Receive6;
+        protected Button Receive7;
 
         //声明已领取文本
-        [SerializeField] protected Text Receive0;
-        [SerializeField] protected Text Receive11;
-        [SerializeField] protected Text Receive22;
-        [SerializeField] protected Text Receive33;
-        [SerializeField] protected Text Receive44;
-        [SerializeField] protected Text Receive55;
-        [SerializeField] protected Text Receive66;
-        [SerializeField] protected Text Receive77;
+        protected Text Receive0;
+        protected Text Receive11;
+        protected Text Receive22;
+        protected Text Receive33;
+        protected Text Receive44;
+        protected Text Receive55;
+        protected Text Receive66;
+        protected Text Receive77;
 
         //声明领取
-        [SerializeField] protected Text Receive00;
-        [SerializeField] protected Text Receive111;
-        [SerializeField] protected Text Receive222;
-        [SerializeField] protected Text Receive333;
-        [SerializeField] protected Text Receive444;
-        [SerializeField] protected Text Receive555;
-        [SerializeField] protected Text Receive666;
-        [SerializeField] protected Text Receive777;
+        protected Text Receive00;
+        protected Text Receive111;
+        protected Text Receive222;
+        protected Text Receive333;
+        protected Text Receive444;
+        protected Text Receive555;
+        protected Text Receive666;
+        protected Text Receive777;
 
 
-        //声明不同段位图片
-        [FormerlySerializedAs("Rank")] [SerializeField]
-        protected Image Rank;
+        //声明不同段位图片，"FormerlySerializedAs"用于防止变量名更改后丢失引用
+        [FormerlySerializedAs("Rank")] protected Image Rank;
 
-        [FormerlySerializedAs("primary")] [SerializeField]
-        protected Sprite primary;
+        [FormerlySerializedAs("primary")] protected Sprite primary;
 
-        [FormerlySerializedAs("master")] [SerializeField]
-        protected Sprite master;
+        [FormerlySerializedAs("master")] protected Sprite master;
 
-        [FormerlySerializedAs("King")] [SerializeField]
-        protected Sprite King;
+        [FormerlySerializedAs("King")] protected Sprite King;
 
         //声明分数面板
-        [SerializeField] protected Text score;
-        [SerializeField] protected Text rankScore;
+        protected Text score;
+        protected Text rankScore;
 
         //声明赛季面板
-        [SerializeField] protected Text saiJi;
+        protected Text saiJi;
         protected int I;
         protected int J;
         protected int Z;
-        [SerializeField] protected Text Sum;
+        protected Text Sum;
     }
 }
